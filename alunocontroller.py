3d2cb204384e38fe373ed831002db0dbb69bb5b7
kalelgf <kalelgomesfreitas06@gmail.com>
2024-10-05 23:03:59 -0300

    atualizacao

--- alunocontroller.py
+++ alunocontroller.py
@@ -1,5 +1,89 @@
 class AlunoController:
 
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def iniciar(self):
+        self.abrir_tela_principal()
+
+    def abrir_tela_principal(self):
+        switcher = {
+            0: self.finalizar,
+            1: self.incluir_aluno,
+            2: self.atualizar_orientador,
+            3: self.registrar_modulo_atual_para_aluno,
+            4: self.remover_modulo_atual_para_aluno,
+            5: self.registrar_modulo_finalizado_para_aluno,
+            6: self.remover_modulo_finalizado_para_aluno,
+            7: self.registrar_certificado_para_aluno,
+            8: self.exibir_detalhes_do_aluno,
+            9: self.exibir_modulos_atuais,
+            10: self.exibir_modulos_finalizados,
+            11: self.exibir_certificados,
+            12: self.exibir_progresso
+        }
+
+        while True:
+            opcao = self.view.mostrar_menu_opcoes()
+            funcao_escolhida = switcher.get(opcao, None)
+            if funcao_escolhida:
+                funcao_escolhida()
+            else:
+                self.view.mostrar_mensagem("Opção inválida!")
+
+    def finalizar(self):
+        self.view.mostrar_mensagem("Encerrando o sistema...")
+        exit(0)
+
+    def incluir_aluno(self):
+        nome, cpf, curso = self.view.obter_dados_aluno()
+        self.model.adicionar_aluno(nome, cpf, curso)
+        self.view.mostrar_mensagem(f"Aluno {nome} incluído com sucesso!")
+
+    def atualizar_orientador(self):
+        orientadores = self.model.get_orientadores()  # Obtém os orientadores já existentes
+        orientador_escolhido = self.view.escolher_orientador(orientadores)
+        if orientador_escolhido:
+            self.model.orientador = orientador_escolhido
+            self.view.mostrar_mensagem(f"Orientador '{orientador_escolhido.nome}' atribuído com sucesso.")
+        else:
+            self.view.mostrar_mensagem("Orientador inválido.")
+
+    def registrar_modulo_atual_para_aluno(self):
+        modulos = self.model.get_modulos()  # Obtém os módulos já existentes
+        modulo_escolhido = self.view.escolher_modulo(modulos)
+        if modulo_escolhido:
+            self.model.estado_aluno.adicionar_modulo_atual(modulo_escolhido)
+            self.view.mostrar_mensagem(f"Módulo '{modulo_escolhido.nome}' adicionado aos módulos atuais.")
+        else:
+            self.view.mostrar_mensagem("Módulo inválido.")
+
+    def remover_modulo_atual_para_aluno(self):
+        modulos_atuais = self.model.estado_aluno.modulos_atuais
+        modulo_escolhido = self.view.escolher_modulo(modulos_atuais)
+        if modulo_escolhido:
+            self.model.estado_aluno.remover_modulo_atual(modulo_escolhido)
+            self.view.mostrar_mensagem(f"Módulo '{modulo_escolhido.nome}' removido dos módulos atuais.")
+        else:
+            self.view.mostrar_mensagem("Módulo inválido.")
+
+    def registrar_modulo_finalizado_para_aluno(self):
+        modulos = self.model.get_modulos()
+        modulo_escolhido = self.view.escolher_modulo(modulos)
+        if modulo_escolhido:
+            self.model.estado_aluno.adicionar_modulo_finalizado(modulo_escolhido)
+            self.view.mostrar_mensagem(f"Módulo '{modulo_escolhido.nome}' adicionado aos módulos finalizados.")
+        else:
+            self.view.mostrar_mensagem("Módulo inválido.")
+
+    def remover_modulo_finalizado_para_aluno(self):
+        modulos_finalizados = self.model.estado_aluno.modulos_finalizados
+        modulo_escolhido = self.view.escolher_modulo(modulos_finalizados)
+        if modulo_escolhido:
+            self.model.estado_aluno.remover_modulo_finalizado(modulo_escolhido)
+            self.view.m
+
     def __init__(self, model: Aluno, view: AlunoView):
         self.model = model
         self.view = view
