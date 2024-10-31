34a4bea8d6c913b6188c25ddfc0073ffd4be410e
Kalel Gomes <kalel@exemplo.com>
2024-10-30 22:05:14 -0300

    fix classe controller e tela questao

--- controllerquestao.py
+++ controllerquestao.py
@@ -1,44 +1,60 @@
 from telaquestao import TelaQuestao
-from questao import Questao 
+from questao import Questao
 
-class ControladorQuestao():
+class ControladorQuestao:
     def __init__(self):
         self.__questoes = []
         self.__tela_questao = TelaQuestao()
 
     def pega_questao_por_id(self, id: int):
         for questao in self.__questoes:
-            if(questao.id == id):
+            if questao.id == id:
                 return questao
-            return None
+        return None
 
     def incluir_questao(self):
         dados_questao = self.__tela_questao.pega_dados_questao()
         q = self.pega_questao_por_id(dados_questao["id"])
+        
         if q is None:
-            questao = Questao(dados_questao["id"], dados_questao["enunciado"],dados_questao["alternativas"],dados_questao["resposta_correta"])
+            questao = Questao(dados_questao["id"], dados_questao["enunciado"],
+                              dados_questao["alternativas"], dados_questao["resposta_correta"])
             self.__questoes.append(questao)
+            self.__tela_questao.mostra_mensagem("Questão adicionada com sucesso!")
         else:
-            self.__tela_questao.mostra_mensagem("ATENCAO: Questao já existente")
+            self.__tela_questao.mostra_mensagem("ATENÇÃO: Questão já existente")
 
     def lista_questao(self):
         for questao in self.__questoes:
-            self.__tela_questao.mostra_questao({"id": questao.id, "enunciado": questao.enunciado})
-        
+            self.__tela_questao.mostra_questao({
+                "id": questao.id,
+                "enunciado": questao.enunciado,
+                "alternativas": questao.alternativas,
+                "resposta_correta": questao.respostas_corretas
+            })
+
     def excluir_questao(self):
         self.lista_questao()
-        id_questao = self.__tela_livro.seleciona_questao()
-        questao = self.pega_livro_por_id(id_questao)
+        id_questao = self.__tela_questao.seleciona_questao()
+        questao = self.pega_questao_por_id(id_questao)
 
-        if(questao is not None):
+        if questao is not None:
             self.__questoes.remove(questao)
+            self.__tela_questao.mostra_mensagem("Questão excluída com sucesso.")
         else:
-            self.__tela_livro.mostra_mensagem("ATENCAO: Questao não existente")
-    
+            self.__tela_questao.mostra_mensagem("ATENÇÃO: Questão não existente")
 
     def abre_tela(self):
-        lista_opcoes = {1: self.incluir_questao, 2: self.excluir_questao}
-
-        continua = True
-        while continua:
-            lista_opcoes[self.__tela_questao.mostrar_menu_opcoes()]()
+        lista_opcoes = {1: self.incluir_questao, 2: self.excluir_questao, 3: self.lista_questao}
+        
+        while True:
+            opcao = self.__tela_questao.mostrar_menu_opcoes()
+            funcao_escolhida = lista_opcoes.get(opcao)
+            
+            if funcao_escolhida:
+                funcao_escolhida()
+            elif opcao == 0:
+                self.__tela_questao.mostra_mensagem("Voltando...")
+                break
+            else:
+                self.__tela_questao.mostra_mensagem("Opção inválida. Tente novamente.")
--- telaquestao.py
+++ telaquestao.py
@@ -1,33 +1,43 @@
-class TelaQuestao():
-
+class TelaQuestao:
     def mostrar_menu_opcoes(self):
         print("-------- QUESTOES ----------")
-        print("Escolha a opcao")
+        print("Escolha a opção")
         print("1. Adicionar Questao")
         print("2. Excluir Questao")
+        print("3. Listar Questões")
         print("0. Voltar")
         opcao = input("Escolha uma opção: ")
         return int(opcao)
 
     def pega_dados_questao(self):
         print("-------- DADOS QUESTAO ----------")
-        id = input("Id: ")
+        id = int(input("Id: "))
         enunciado = input("Enunciado: ")
-        alternativas = input("Alternativas: ")
-        resposta_correta = input("Resposta Correta: ")
+        alternativas = input("Alternativas (separadas por vírgula): ").split(", ")
+        resposta_correta = [input("Resposta Correta (exatamente como aparece nas alternativas): ")]
+        return {
+            "id": id,
+            "enunciado": enunciado,
+            "alternativas": alternativas,
+            "resposta_correta": resposta_correta
+        }
 
     def mostra_mensagem(self, msg):
         print(msg)
 
     def seleciona_questao(self):
-        id = input("Código da Questao que deseja selecionar: ")
+        id = int(input("Código da Questao que deseja selecionar: "))  # Converte o ID para inteiro
         return id
+
     
-    def mostra_questao(self, dados_questao ):
+    def mostra_questao(self, dados_questao):
+        print("-------- QUESTAO ----------")
         print("ID DA QUESTAO: ", dados_questao["id"])
         print("ENUNCIADO DA QUESTAO: ", dados_questao["enunciado"])
-        print("ALTERNATIVAS DA QUESTAO: ", dados_questao["alternativas"])
-        print("RESPOSTA CORRETA: ", dados_questao["resposta_correta"])
+        print("ALTERNATIVAS DA QUESTAO: ", ", ".join(dados_questao["alternativas"]))
+        print("RESPOSTA CORRETA: ", ", ".join(dados_questao["resposta_correta"]))
         print("\n")
+
+
 
     
